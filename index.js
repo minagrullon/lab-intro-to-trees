99fbb7654ec31b7703fa3139b2388c60e07fa540
minagrullon <minagrullon@pursuit.org>
2023-03-31 13:27:02 -0400

    passes tests

--- index.js
+++ index.js
@@ -10,7 +10,33 @@ class Tree {
     this.root = node;
   }
 
-  findNode(data) {}
+  findNode(data) {
+    // if (!this.root || !this.root.children) {
+    //   return null;
+    // }
+    // if (this.root.data === data) {
+    //   return this.root;
+    // }
+    // let node = this.root
+    // const result = node.children.
+
+    // return result;
+    let stack = [];
+    let node = this.root;
+    stack.push(node);
+
+    while (stack.length > 0) {
+      node = stack.pop();
+      if (node.data === data) {
+        return node;
+      } else if (node.children.length > 0) {
+        for (let i = 0; i < node.children.length; i++) {
+          stack.push(node.children[i]);
+        }
+      }
+    }
+    return null;
+  }
 }
 
 module.exports = { TreeNode, Tree };
